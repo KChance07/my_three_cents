81af76e06246c5432a22cdc622765385b59d5818
Kyle Chance <kylechance@gmail.com>
2016-11-25 11:51:46 -0500

    saving and stuff

--- server/routers/user.router.js
+++ server/routers/user.router.js
@@ -1,12 +1,40 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../model/user.model.js');
+var User = require('../models/user.model.js');
 
-router.get('users/profile/:userId', function(req, res){});
-router.post('users/signup', function(req, res){});
-router.post('users/login', function(req, res){});
-router.put('users/profile/:userId', function(req, res){});
+router.get('users/profile/:userId', function(req, res){
+  User.find({ _id: req.params.userId }, function(err, user){
+    if(err){
+      return res.status(500).json({
+        err: err
+      });
+    }
+    return res.status(200).json({
+      user: user
+    });
+  });
+});
+
+router.post('users/signup', function(req, res){
+  var user = new User(req.body);
+  user.save(function(err){
+    if(err){
+      return res.status(500).json({
+        err: err
+      });
+    }
+    return res.status(201).json({
+      msg: 'Success!'
+    });
+  });
+});
+
+router.post('users/login', function(req, res){
+
+});
+
+router.put('users/profile/:userId', function(req, res){
+
+});
 
 module.exports = router;
-
- 
