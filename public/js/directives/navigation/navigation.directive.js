b0bd6bb11b8d0b013f2bec623e8e64cee093f8c2
Kyle Chance <kylechance@gmail.com>
2016-12-05 19:41:28 -0500

    created initial top nav menu and some styling

--- public/js/directives/navigation/navigation.directive.js
+++ public/js/directives/navigation/navigation.directive.js
@@ -1,3 +1,4 @@
+
 (function() {
   angular.module('two-cents')
     .directive('navigation', NavigationDirective);
@@ -7,6 +8,7 @@
     function NavigationDirective(){
       return {
         restrict: 'EA',
+        scope: {},
         templateUrl: 'js/directives/navigation/navigation.view.html',
         controller: 'NavigationController'
       }
